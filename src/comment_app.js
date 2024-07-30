2567917623072722e3c25dcc20bd3c087fa991e5
Atoom <leon.dekshtein@gmail.com>
2024-07-30 22:34:07 +0300

    add comment_app.js file

---
+++ src/comment_app.js
@@ -0,0 +1,61 @@
+
+async function fetchComments() {
+    const url = 'https://hutly-landing-page-back.onrender.com/api/comments';
+
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+
+        const comments = await response.json();
+        console.log('Fetched comments:', comments);
+
+        comments.forEach(comment => {
+            postComment(comment);
+        });
+
+    } catch (error) {
+        console.error('Failed to fetch comments:', error);
+    }
+}
+
+function postComment(comment) {
+    const fN = comment.firstName || 'Anonymous';
+    const lN = comment.lastName || 'Anonymous';
+    const rate = comment.rate || 0;
+    const text = comment.text || 'No content';
+
+    console.log(fN, lN, rate, text);
+
+    const post = document.getElementsByClassName('testimonials-container');
+    const postElement = document.createElement('div');
+    postElement.className = 'comment';
+
+
+    const fN_Element = document.createElement('div');
+    fN_Element.className = 'comment-fN';
+    fN_Element.textContent = fN;
+
+    const lN_Element = document.createElement('div');
+    lN_Element.className = 'comment-lN';
+    lN_Element.textContent = lN;
+
+    const textElement = document.createElement('div');
+    textElement.className = 'comment-text';
+    textElement.textContent = text;
+
+    const rateElement = document.createElement('div');
+    rateElement.className = 'comment-rate';
+    rateElement.textContent = `rate: ${rate}`;
+
+    postElement.appendChild(fN_Element);
+    postElement.appendChild(lN_Element);
+    postElement.appendChild(textElement);
+    postElement.appendChild(rateElement);
+
+    // post.appendChild(postElement);
+}
+
+fetchComments();
+
